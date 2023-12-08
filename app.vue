ee2c2ca8637d75ea818adb670a6506a575c59b20
Kenny Ramin <kennyramin@MacBook-Air-de-Kenny.local>
2023-12-08 13:07:23 +0100

    config nuxt 3 + tailwindcss + daisyUI

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div> 
+    <!-- <NuxtWelcome /> -->
+    <div class="flex justify-center h-screen space-x-4 items-center">
+      <div class="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img src="https://images.assetsdelivery.com/compings_v2/kapona/kapona2111/kapona211100019.jpg" alt="Shoes" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
