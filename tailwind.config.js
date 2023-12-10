a2396a2f886ca1be19d3da27671f12193627df07
Kenny Ramin <kennyramin@MacBook-Air-de-Kenny.local>
2023-12-10 14:47:24 +0100

    home + projects sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,15 @@
 module.exports = {
     plugins: [require("daisyui")],
+    daisyui: {
+        themes: ["light", "dark"],
+      },
+      theme: {
+        fontFamily: {
+          'sans': ['raleway'],
+          'serif':  ['raleway'],
+          'mono':  ['raleway'],
+          'display':  ['raleway'],
+          'body':  ['raleway'],
+        }
+      }
   };
