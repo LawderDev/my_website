164093c278de114a252051da9e54370486627b23
Kenny Ramin <kenny.ramin433@gmail.com>
2024-02-13 09:24:44 +0100

    keep lotties canvas

--- components/HomeSection.vue
+++ components/HomeSection.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex items-center justify-center min-h-screen overflow-clip pt-10 z-1">
         <div v-if="isDesktop" class="w-[353px] hidden lg:flex" data-aos="slide-left">
-            
+            <canvas ref="lottieAnimate2" class="w-[300px] h-[300px]"></canvas>
         </div>
         <div class="relative" data-aos="fade-up">
-         
+          <canvas ref="lottieAnimate" class="w-[512px] h-[512px]"></canvas>
           <img src="/me.svg" alt="me" class="absolute top-20 left-24" />
   
           <div class="relative bottom-14">
@@ -12,11 +12,11 @@
               <span class="text-primary">Kenny Ramin,</span> Développeur
             </h1>
           </div>
-        
+          <canvas ref="lottieNext" class="w-[150px] h-[150px] relative bottom-14 left-48"></canvas>
         </div>
   
         <div v-if="isDesktop" data-aos="slide-right" class="hidden lg:flex">
-        
+          <canvas ref="lottieAnimate3" class="w-[400px] h-[400px] relative"></canvas>
         </div>
       </div>
 </template>
@@ -29,7 +29,7 @@ const lottieAnimate3 = ref(null)
 const lottieNext = ref(null)
 
 const initLotties = () => {
- /* new DotLottie({
+  new DotLottie({
 	  canvas: lottieAnimate.value,
 	  src: "/lotties/animate.lottie",
 	  loop: true,
@@ -41,7 +41,7 @@ const initLotties = () => {
 	  src: "/lotties/next.lottie",
 	  loop: true,
 	  autoplay: true
-	});*/
+	});
 }
 
 const initConditionalLotties = () => {
