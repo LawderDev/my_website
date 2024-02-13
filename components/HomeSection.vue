9e780ccbc9008c61901a43648d824cdea36c59af
Kenny Ramin <kenny.ramin433@gmail.com>
2024-02-13 09:13:40 +0100

    improve lotties

--- components/HomeSection.vue
+++ components/HomeSection.vue
@@ -1,19 +1,10 @@
 <template>
     <div class="flex items-center justify-center min-h-screen overflow-clip pt-10 z-1">
         <div v-if="isDesktop" class="w-[353px] hidden lg:flex" data-aos="slide-left">
-          <Vue3Lottie
-            renderer="svg"
-            :animationData="animate2"
-            :width="300"
-          />
+            <canvas ref="lottieAnimate2" class="w-[300px] h-[300px]"></canvas>
         </div>
         <div class="relative" data-aos="fade-up">
-          <Vue3Lottie
-            renderer="svg"  
-            :animationData="animate"
-            :width="512"
-          />
-  
+          <canvas ref="lottieAnimate" class="w-[512px] h-[512px]"></canvas>
           <img src="/me.svg" alt="me" class="absolute top-20 left-24" />
   
           <div class="relative bottom-14">
@@ -21,35 +12,62 @@
               <span class="text-primary">Kenny Ramin,</span> Développeur
             </h1>
           </div>
-  
-          <Vue3Lottie
-            class="relative bottom-14"
-            renderer="svg"
-            :animationData="nextAnimation"
-            :height="150"
-            :width="150"
-          />
+          <canvas ref="lottieNext" class="w-[150px] h-[150px] relative bottom-14 left-48"></canvas>
         </div>
   
-        <div>
-          <Vue3Lottie
-            v-if="isDesktop"
-            renderer="svg"
-            class="hidden lg:flex"
-            data-aos="slide-right"
-            :animationData="animate3"
-            :width="400"
-          />
+        <div v-if="isDesktop" data-aos="slide-right">
+          <canvas ref="lottieAnimate3" class="w-[400px] h-[400px] relative"></canvas>
         </div>
       </div>
 </template>
 
 <script setup>
-import { Vue3Lottie } from 'vue3-lottie'
-import animate from '~/assets/lotties/animate.json'
-import animate2 from '~/assets/lotties/animate2.json'
-import animate3 from '~/assets/lotties/animate3.json'
-import nextAnimation from '~/assets/lotties/next.json'
+import { DotLottie } from "https://esm.sh/@lottiefiles/dotlottie-web";
+const lottieAnimate = ref(null)
+const lottieAnimate2 = ref(null)
+const lottieAnimate3 = ref(null)
+const lottieNext = ref(null)
 
-const isDesktop = () => window.matchMedia('(min-width: 1024px)').matches
+const initLotties = () => {
+  new DotLottie({
+	  canvas: lottieAnimate.value,
+	  src: "/lotties/animate.lottie",
+	  loop: true,
+	  autoplay: true
+	});
+
+  new DotLottie({
+	  canvas: lottieNext.value,
+	  src: "/lotties/next.lottie",
+	  loop: true,
+	  autoplay: true
+	});
+}
+
+const initConditionalLotties = () => {
+  if(lottieAnimate2.value){
+    new DotLottie({
+	  canvas: lottieAnimate2.value,
+	  src: "/lotties/animate2.lottie",
+	  loop: true,
+	  autoplay: true
+	});
+  }
+
+  if(lottieAnimate3.value){
+    new DotLottie({
+	  canvas: lottieAnimate3.value,
+	  src: "/lotties/animate3.lottie",
+	  loop: true,
+	  autoplay: true
+	});
+  }
+}
+
+onMounted(() => {
+  initLotties()
+  initConditionalLotties()
+})
+
+const isDesktop = window.matchMedia('(min-width: 1024px)').matches
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  tailwindcss: {
-    purge: true,
+  vue: {
+    compilerOptions: {
+      isCustomElement: (tag) => ['dotlottie-player'].includes(tag),
+    },
   },
 })
