96813f0c4c20f141deb8115e583ebebd39ffe574
Kenny Ramin <kenny.ramin433@gmail.com>
2024-02-23 12:03:57 +0100

    update resume

--- components/ContactButton.vue
+++ components/ContactButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col justify-center items-center pt-40 pb-24 relative z-1 gap-5">
     <a href="mailto:kenny.ramin433@gmail.com">
-      <button class="btn btn-primary animate-bounce-custom">Contact me !</button>
+      <button class="btn btn-primary animate-bounce-custom">Me contacter !</button>
     </a>
     <div class="flex gap-5">
       <a class="btn p-0 animate-bounce-custom" href="https://www.linkedin.com/in/kenny-ramin/"><img alt="Linkedin" class="icon-size" src="/icons/linkedIn.svg"/></a>
